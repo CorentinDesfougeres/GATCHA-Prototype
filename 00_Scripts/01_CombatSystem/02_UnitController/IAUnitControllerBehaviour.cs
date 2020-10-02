f8626bd41e115af18593b7156757c8c94edc0ea2
CorentinDesfougeres <72070778+CorentinDesfougeres@users.noreply.github.com>
2020-10-03 00:51:20 +0200

    IAUnitController
    Script created

---
+++ 00_Scripts/01_CombatSystem/02_UnitController/IAUnitControllerBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAUnitControllerBehaviour : UnitControllerBehaviour
+{
+    public override void PickAction()
+    {
+
+    }
+
+}
