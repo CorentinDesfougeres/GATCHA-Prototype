ef7ecf9823c4260e09a274d9ea4fa8f315c5ac2b
CorentinDesfougeres <72070778+CorentinDesfougeres@users.noreply.github.com>
2020-10-21 23:58:54 +0200

    Cooldown
    still empty, just placeholder

--- 00_Scripts/02_UnitSystem/01_ActionSystem/01_Effect/APCost.cs
+++ 00_Scripts/02_UnitSystem/01_ActionSystem/01_Effect/APCost.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 using System;
 
 [Serializable]
-public class CoolDown : Effect
+public class APCost : Effect
 {
+    public int Amount;
     public override void Execute(ActorBehaviour _source , UnitBehaviour[] _targets)
     {
-        // put the source on cooldown
+        foreach (UnitBehaviour _target in _targets)
+        {
+            _target.ActionPoints -= Amount;
+        }
     }
 }
